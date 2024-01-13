ec43724736c1f830959554666ffabe19775d5ec8
nataliasov <120122321+nataliasov@users.noreply.github.com>
2024-01-13 17:27:17 -0600

    Initial files

---
+++ firstprojecthmjan13/src/main/java/com/itexps/training/firstprojecthmjan13/FirstProjectH.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itexps.training.firstprojecthmjan13;
+
+/**
+ *
+ * @author Natalia
+ */
+public class FirstProjectH {
+    public int add (int x, int y) {
+        return x + y;
+    }
+     public int subtract (int x, int y) {
+        return y - x;
+     }
+}
